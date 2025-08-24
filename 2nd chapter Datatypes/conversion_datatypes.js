655279f12cead6470d8e6c2cbd8ba398a831963a
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2025-08-24 22:41:14 +0530

    Only Discuss Primitive Data types

---
+++ 2nd chapter Datatypes/conversion_datatypes.js
@@ -0,0 +1,9 @@
+/**
+ * ! Critical: This function handles user authentication
+ * ? Should we add rate limiting here?
+ * TODO: Implement OAuth2 support
+ * * Main authentication logic
+ */
+function authenticate(user) {
+    // Function implementation
+}
---
+++ 2nd chapter Datatypes/datatypes.js
@@ -0,0 +1,57 @@
+
+/*******  Data Types in JS ****************/
+/* Mainly  used 2 types of datatypes used
+    1. primitive
+    2. non primitive
+
+    in primitive 7 Types datatypes used in Javascript
+    N=Null
+    N=Number
+    S=symbol
+    S=String
+    B=Boolean
+    B=BigInt
+    U=undefined
+
+*/
+// Primitive Data Types in JavaScript
+
+// 1. Null
+let n = null;
+console.log("Null:", n);
+
+// 2. Number
+let num = 123;
+console.log("Number:", num);
+
+// 3. Symbol
+let sym = Symbol("id");
+console.log("Symbol:", sym);
+
+// 4. String
+let str = "Hello JavaScript";
+console.log("String:", str);
+
+// 5. Boolean
+let isTrue = true;
+console.log("Boolean:", isTrue);
+
+// 6. BigInt (for very large numbers)
+let big = 123456789012345678901234567890n;
+console.log("BigInt:", big);
+
+// 7. Undefined (when value not assigned)
+let u;
+console.log("Undefined:", u);
+
+
+
+/*  
+//* in Non primitive 3 Types datatypes used in Javascript
+    1. Arrays
+    2. Objects
+    3. Functions 
+*/
+
+
+
