8aa3366f6935c5e176d0b8a62982e9ca2c4143b7
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2025-08-30 20:29:13 +0530

    Important Rules in Javascript Object discuss fundamental  rule

--- 2nd chapter Datatypes/conversion_datatypes.js
+++
@@ -1,9 +0,0 @@
-/**
- * ! Critical: This function handles user authentication
- * ? Should we add rate limiting here?
- * TODO: Implement OAuth2 support
- * * Main authentication logic
- */
-function authenticate(user) {
-    // Function implementation
-}
--- 2nd chapter Datatypes/datatypes.js
+++ 2nd chapter Datatypes/datatypes.js
@@ -1,6 +1,7 @@
 
-/*******  Data Types in JS ****************/
-/* Mainly  used 2 types of datatypes used
+//*******  Data Types in JS ****************/
+/*
+   Mainly  used 2 types of datatypes used
     1. primitive
     2. non primitive
 
@@ -14,44 +15,73 @@
     U=undefined
 
 */
-// Primitive Data Types in JavaScript
+//* Primitive Data Types in JavaScript
 
 // 1. Null
 let n = null;
-console.log("Null:", n);
+//console.log("Null:", n);
 
 // 2. Number
 let num = 123;
-console.log("Number:", num);
+//console.log("Number:", num);
 
 // 3. Symbol
 let sym = Symbol("id");
-console.log("Symbol:", sym);
+//console.log("Symbol:", sym);
 
 // 4. String
 let str = "Hello JavaScript";
-console.log("String:", str);
+//console.log("String:", str);
 
 // 5. Boolean
 let isTrue = true;
-console.log("Boolean:", isTrue);
+//console.log("Boolean:", isTrue);
 
 // 6. BigInt (for very large numbers)
 let big = 123456789012345678901234567890n;
-console.log("BigInt:", big);
+//console.log("BigInt:", big);
 
 // 7. Undefined (when value not assigned)
 let u;
-console.log("Undefined:", u);
+//console.log("Undefined: becose no declare u", u);
 
-
-
+// Another way print all Variables
+//console.log(n,num,sym,str,isTrue,big,u);
 /*  
-//* in Non primitive 3 Types datatypes used in Javascript
+ * in Non primitive 3 Types datatypes used in Javascript
     1. Arrays
     2. Objects
     3. Functions 
 */
+//* here only Discuss Objects later Discuss Arrays and funtions in another files
+const item = 
+{
+    Roll :123879,
+    lovish:false,
+    name :"kirti",
+    age:32,
+    roll_no:67878,
+    Rollno:98760
+   // in identifier or variable name using space or startring with no show error Example like Roll no here use gap after roll and starting with numer shows same error 
+}
+//console.log(item);
 
+//*Another Way call spececic property in object
 
+const test = 
+{
+    Roll :123879,
+    lovish:false,
+    name :"kirti",
+    age:32,
+    section: "B",
+    location:"chhatia",
+    lastloggedindays:["Monday", "Tuesday"]
 
+}
+//console.log(test.lovish);
+//*Right Way call specific property in object in companies
+console.log(test["lovish"]);
+
+//! under object property Automatic Consider String Example Here Roll automaticaly consider "Roll"Like it Automatically Convert Section = "Section"
+// so when access by Dot Opertor (test.lovish) no need to write Lovish into ""double Quoted line but in Square bracket test["lovish"] need lovish into Double Quoted Line 
