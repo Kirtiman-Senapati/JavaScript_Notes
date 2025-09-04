3e0c1af65129845bcda5b53ff6ff81b4c08ac601
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2025-09-04 23:24:15 +0530

    Here Discuss  Data tyoes Conversion for interview

--- 2nd chapter Datatypes/datatypesconversion.js
+++ 2nd chapter Datatypes/datatypesconversion.js
@@ -1,5 +1,5 @@
 let age = 70;
-let name = "Kirtiman"
+let name = true
 //console.log(typeof age);
 //console.log(typeof(age));
 
@@ -7,8 +7,63 @@ let name = "Kirtiman"
 
 
 let Valuechange_in_boolean = Boolean(age)
-console.log(typeof Valuechange_in_boolean);
+//console.log(typeof Valuechange_in_boolean);
 
 
 let ValueinNumber = Number(name)
-console.log(typeof(ValueinNumber));
+//console.log(ValueinNumber);
+
+//console.log(typeof(ValueinNumber));
+
+// check new varible
+
+let isloggedIn = ""
+let valueinboolean = Boolean(isloggedIn)
+//console.log(valueinboolean);
+//console.log(typeof valueinboolean);
+
+
+
+// check new varible for string
+
+let score = undefined
+let chagein_String =String (score)
+console.log(chagein_String);
+console.log(typeof chagein_String);
+
+
+//* Here Discuss  Data tyoes Conversion for interview
+
+/* 1. 123 Kirtiman and kirtiman 123 converted to easily number show result when using typeof command but when want to which value assign in variable show use console.log command it shows result NaN, NaN means not a number
+
+2. when using null value print Value '0' and typeof number
+
+3. when using undefined print Value '0' and typeof number
+
+4. when using boolean value true print Value '1' and typeof number
+
+5. when using boolean value false print Value '0' and typeof number
+
+6. when using number value 1 print Value 'true' and typeof boolean in boolean case
+
+7.  when using number value 0 print Value 'false' and typeof boolean in boolean case
+
+8.  when using string value print Value 'true' and typeof boolean in boolean case
+
+9. when using kirtiman123 value print Value 'true' and typeof boolean in boolean case
+
+10. when using undefined  print Value 'false' and typeof boolean in boolean case
+
+11.  when using Null print Value 'false' and typeof boolean in boolean case
+
+12.  when using ""  print Value 'false' and typeof boolean in boolean case
+
+13.  when using number  print Value 'number' and typeof string in string case
+
+14. when using kirti123  print Value 'kirti123' and typeof string in string case
+
+15. when using null  print Value ''null' and typeof string in string case
+
+16. when using undefined print Value 'undefined' and typeof string in string case
+*/
+
