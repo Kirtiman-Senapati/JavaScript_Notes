da4120eba84f77eca1d615c604289e81bbdbe9a0
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2025-09-04 22:38:05 +0530

    Here Discuss  Data tyoes Conversion

--- 2nd chapter Datatypes/datatypesconversion.js
+++ 2nd chapter Datatypes/datatypesconversion.js
@@ -1,5 +1,14 @@
 let age = 70;
 let name = "Kirtiman"
-console.log(typeof age);
-console.log(typeof(age));
+//console.log(typeof age);
+//console.log(typeof(age));
 
+//* Here Discuss  Data tyoes Conversion
+
+
+let Valuechange_in_boolean = Boolean(age)
+console.log(typeof Valuechange_in_boolean);
+
+
+let ValueinNumber = Number(name)
+console.log(typeof(ValueinNumber));
