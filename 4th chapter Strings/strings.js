e5823ea6b12d92d79ebed8ab6350e99478d2cc51
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2025-09-05 22:50:36 +0530

    string Operation interview Questions

--- 4th chapter Strings/strings.js
+++ 4th chapter Strings/strings.js
@@ -4,4 +4,22 @@
 let str1 ="kirti"
 let str2 ="man"
 let str3 = str1 + str2
-console.log(str3);
+//console.log(str3);
+
+
+//* interview Question
+
+// console.log(typeof "1" + 2);//result 12 //string2
+
+// console.log(typeof 1 + "2");//result 12 //number2
+
+// console.log(typeof "1"+"2");//result 12 // string2
+
+// console.log(typeof "1" + 2 + 2);//result 122 // string22
+
+// console.log(typeof 1 + 2 + "2");// result 32 // number 22
+console.log(+ true); // value print 1
+console.log(typeof + true); // type number
+console.log(typeof + ""); //value print 0 // type number
+
+
