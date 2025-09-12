05c813cde1ee41c22c67bed1f443716ed8fd062d
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2025-09-12 22:20:46 +0530

    Discuss Local String method use country standard

--- 1st chapter Variables/number&math.js
+++ 1st chapter Variables/number&math.js
@@ -18,3 +18,25 @@ console.log(balance.toFixed(2));//to fixed use show fixed decimal value
 
 
 
+const otherNumber = 1145.8987
+
+console.log(otherNumber.toPrecision(4));
+//input 45.0987 output 45.1 because here focuses 45.0
+
+//when input 145.0987 show output 145 because it focuses 3 values 145 
+
+
+//when input 145.8987 show output 146 because it focuses 3 values 145
+
+
+
+//when input 1145.0987 show output 1.15e+3 because it focuses 3 values 145
+
+
+
+//* Discuss Local String
+const hundreds = 10000000
+console.log(hundreds.toLocaleString());// use to comma in number and it used to particular country value/ standard 
+
+console.log(hundreds.toLocaleString("en-IN"));
+
