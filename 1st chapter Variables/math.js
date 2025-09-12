5ead0532e86125ec2fbd89ce95f887d5a1d5dd09
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2025-09-12 23:19:24 +0530

    Discuss math random for game design website

--- 1st chapter Variables/math.js
+++ 1st chapter Variables/math.js
@@ -1,1 +1,41 @@
 /* ************** Math in JS***************** */
+
+
+//const Result = 90%
+
+//console.log(Math);
+
+//console.log(Math.abs());//abs means positive means absolute value
+
+//console.log(Math.round(5.6));// use display round of value
+
+//console.log(Math.ceil(5.6));// it returns top value means if u want write output 6 then use it takes grater value
+
+//console.log(Math.floor());// it returns bottom value means if u want write output 4 then use it
+
+//console.log(Math.max(45,89,76,75));// write max value from array
+
+
+//console.log(Math.min(45,89,76,75));// write minimum value from array
+
+
+//console.log(Math.random());// returns seudo random no between 0,1
+
+
+//console.log(Math.random()*10 + 1 );// very confusing ranges value //only shows above 0 value
+
+// problems in add and subtract show use this way
+
+//console.log(Math.floor(Math.random()*10) + 1 );
+
+
+//trick for checking ranges in gamming design website
+
+
+const min = 10
+const max = 20
+
+console.log(Math.random()  *  (max - min + 1));
+
+
+console.log(Math.floor((Math.random()  *  (max - min + 1)))) + min
