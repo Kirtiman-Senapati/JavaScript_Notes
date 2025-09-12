1b72865111fdecff1cc6e5a9a9fb1fb1fd57e4e0
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2025-09-12 21:57:52 +0530

    Discuss number & math and  use methods in numbers

---
+++ 1st chapter Variables/number&math.js
@@ -0,0 +1,20 @@
+const score = 300
+
+console.log(score);
+
+
+const balance = new Number(100)  //here object use through new keyword
+
+console.log(balance);
+
+
+// * use methods in number
+
+console.log(balance.toString().length);
+
+//to string() used convert number property to string then easily used string method
+
+console.log(balance.toFixed(2));//to fixed use show fixed decimal value
+
+
+
