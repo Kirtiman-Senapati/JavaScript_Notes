84ac9615a8f3c8e2dd321967467bb1b6201a55fd
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2025-09-14 23:21:01 +0530

    Another Way right Same code Means Funtion As Arguments

---
+++ 3rd chapter Functions/functions.js
@@ -0,0 +1,22 @@
+function add(){
+    const a =19
+    const b = 20
+    let value = a + b ;
+    console.log(value);
+    
+}
+
+add()
+
+
+// Another Way right Same code Means Funtion As Arguments
+
+
+function addTwoNumbers(number1, number2)
+{
+    console.log(number1 + number2);
+
+    
+}
+
+addTwoNumbers(19,20)
