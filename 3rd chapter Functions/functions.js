ed8daf9c78c96256992cba553327294688d8faa6
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2025-09-15 01:06:21 +0530

    here Discuss how use parameter or arguments better way

--- 3rd chapter Functions/functions.js
+++ 3rd chapter Functions/functions.js
@@ -2,7 +2,7 @@ function add(){
     const a =19
     const b = 20
     let value = a + b ;
-    //console.log(value);
+    console.log(value);
     
 }
 
@@ -14,15 +14,15 @@ add()
 
 function addTwoNumbers(number1, number2)
 {
-    //console.log(number1 + number2);
+    console.log(number1 + number2);
 
     
 }
 
-const result = addTwoNumbers(19,20)// not Returned Value So not stored value in result so //console .log print output undefined   
+const result = addTwoNumbers(19,20)// not Returned Value So not stored value in result so console .log print output undefined   
 
 
-//console.log(`result is ${result}`);
+console.log(`result is ${result}`);
 
 
 
@@ -35,8 +35,7 @@ function addTwoNumbers(number1, number2)
 {
     
     let result = number1 + number2
-    return result
-    console.log("kirtiman");// after return code not work because after return keyword function finish if you want print console.log kirtiman then write it before return 
+    return resultconsole.log("kirtiman");// after return code not work because after return keyword function finish if you want princonsole.log kirtiman then write it before return 
     
 }
 
@@ -56,3 +55,30 @@ function addTwoNumbers(number1, number2)
 const Result2 = addTwoNumbers(100,700)
 
 console.log(`Result 2 is ${Result2}`);
+
+
+// * here Discuss how use parameter or arguments better way
+
+function loginUserMessage(username)
+{
+    if(username === undefined)
+    {
+console.log("user not declared please entre a user name");
+        
+    }
+    return`${username} logged in sucessfully`
+    
+}console.log(loginUserMessage())
+
+
+
+console.log (loginUserMessage());// undefined user logged in sucessfully
+
+
+console.log (loginUserMessage("")); //"" user logged in sucessfully
+
+
+console.log (loginUserMessage("kirtiman")); // kirtiman user logged in sucessfully
+
+//*username === undefined is equal to ! if i write if(!username) same with username === undefined because ! change the values trues values false false values true
+
