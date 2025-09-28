a0093ca95c50d2979cddd13f9d25ac089895ffbb
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2025-09-29 02:01:08 +0530

    Discuss Switch case

---
+++ 7th chapter Condition_Statement/switchcase.js
@@ -0,0 +1,62 @@
+
+// let option = prompt("Enter a option no")
+
+// switch (option) {
+//     case 1:
+//     {
+//         console.log("Namaste");
+//     }
+//     break;
+    
+
+//     case 2:
+//     {
+//         console.log("Hello");
+//     }
+//     break;   
+  
+
+//     case 3:
+//     {
+//         console.log("GoodMorning");
+           
+//     }
+//     break;
+// }
+
+//* in switch case i use case nos like no 1,no2,no3 but prompt consider enter input is string so not provide error or output
+
+
+
+let correction = Number(prompt("Enter a option no"))
+
+switch (correction) 
+{
+    case 1:
+    {
+        console.log("Namaste");
+    }
+    break;
+    
+
+    case 2:
+    {
+        console.log("Hello");
+    }
+    break;   
+  
+
+    case 3:
+    {
+        console.log("GoodMorning");
+           
+    }
+    break;
+
+    default:
+    {
+        console.log("Invalid OPtion");
+    
+    }
+
+}
---
+++ 7th chapter Condition_Statement/tempCodeRunnerFile.js
@@ -0,0 +1,25 @@
+
+let option = prompt("Enter a option no")
+
+switch (option) {
+    case 1:
+    {
+        console.log("Namaste");
+    }
+    break;
+    
+
+    case 2:
+    {
+        console.log("Hello");
+    }
+    break;   
+  
+
+    case 3:
+    {
+        console.log("GoodMorning");
+           
+    }
+    break;
+}
