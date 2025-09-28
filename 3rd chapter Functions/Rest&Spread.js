baa466b46d7f014d9ed7c312d6869f0a0e22128c
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2025-09-28 20:18:16 +0530

    Discuss Spread in Functions

--- 3rd chapter Functions/Rest&Spread.js
+++ 3rd chapter Functions/Rest&Spread.js
@@ -10,3 +10,24 @@ function addNumbers(num1,num2,num3,...other)
 
 }
 console.log(addNumbers(60,30,10,80,20))
+
+
+//* Spread means combine to divide 
+
+// Example 
+
+let names = ["Ajay","Anuj","Rohan"];
+
+function getNames(name1,name2,name3)
+{
+    console.log(name1,name2,name3);
+    
+}
+
+getNames(names[0],names[1],names[2])
+
+//Advance way
+
+getNames(...names)
+
+getNames(names)
