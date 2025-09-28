840b95a303288a1fb766233da0fc1f8caedb7cf9
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2025-09-29 01:13:12 +0530

    Discuss if and else if

--- 7th chapter Condition_Statement/if_else.js
+++ 7th chapter Condition_Statement/if_else.js
@@ -2,15 +2,22 @@
 
 let age = prompt("enter your age")
 
-if (age>=18) 
+if (age>=80) 
+{
+    console.log("Yes you are Adult");
+        
+}
+
+else if(age >= 18)
+
 {
     console.log("Yes you can vote");
-        
+    
 }
 
 else
 {
-    console.log("No you can vote");
+    console.log("No you cannot vote");
     
 }    
 
