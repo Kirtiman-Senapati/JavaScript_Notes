6a111760773c17230eee0d690707f0749bbe82ff
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2026-09-04 22:23:55 +0530

    discuss splice  methods

--- 9th chapter Array/array.js
+++ 9th chapter Array/array.js
@@ -70,7 +70,7 @@ console.log(vegetables)//output ["carrot","onion","potato,ladiis finger","cucumm
 console.log(vegetables)//[ 'carrot', 'onion', 'potato', 'ladiis finger', 'cucummber' ] only delete beat element 
 
 
-vegetables.pop("ladiis finger")//wrong provide argument 
+vegetables.pop("ladiis finger")//wrong provide parameter 
 
 console.log(vegetables) //[ 'carrot', 'onion', 'potato', 'ladiis finger' ] only delete item from last if give arguments or not 
 
@@ -88,3 +88,22 @@ vegetables.unshift("pumpkin")
 console.log(vegetables)//[ 'pumpkin','onion', 'potato', 'ladiis finger' ] add pumpkin from starting 
 
 
+
+
+
+/* ******************************************** Splice method ********************************************** */
+
+//eea method delete karta hai specific items ko ya range ko
+
+let arr =["Ankit","Rohan","Ayush","kirtiman","cheeranjeeb"]
+
+arr.splice(2)//here index  2 se elements katna start kiya end tak because gives 1 argument only start index 2
+
+console.log(arr)
+
+
+let arr2 =["Ankit","Rohan","Ayush","kirtiman","cheeranjeeb"]
+
+arr2.splice(1,3)//here index  1 se elements katna start kiya index 3 tak only 2 elements deleted hamesha last parameter -1 tak elements delete karega 
+
+console.log(arr2)  //['Ankit,'cheeranjeeb'] because provides parameter 1,3 so only 2 elements index 1 and index 2 ka
